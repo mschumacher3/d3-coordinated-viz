174c4fd29fa8da05215962f0e1e41d2f2ab90add
mschumacher3 <mschumacher3@wisc.edu>
2016-03-30 11:19:33 -0500

    Module 8 saving

--- js/main.js
+++ js/main.js
@@ -1,21 +1,50 @@
-//D3 DEMO
+
 //begin script when window loads
 window.onload = setMap();
 
-//set up choropleth map
+//sets up choropleth map
 function setMap(){
     //set width, height
-    var width=1050, height=550;
+    var width=1050, 
+        height=550;
+
+      //creates an Albers equal area conic projection for the US
+    var projection = d3.geo.albersUsa()
+        .rotate([96, 0])
+        .center([-.6, 38.7])
+        .parallels([29.5, 45.5])
+        .scale(1070)
+        .translate([width / 2, height / 2])
+
+    var path = d3.geo.path()
+        .projection(projection);
+        
     //use queue.js to parallelize asynchronous data loading
-    var q = d3_queue.queue();
+    var q = d3_queue.queue();   
+        q
         .defer(d3.csv, "data/StateData.csv") //load attributes from csv
         .defer(d3.json, "data/USAStates.topojson") //load choroplethspatial data
         .await(callback);
 
-    function callback(error, csvData, europe, france){
-        console.log(error);
-        console.log(csvData);
-        console.log(europe);
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+  
+
+    function callback(error, csvData, us){
+        //translate the Counties to topojson
+        var usUSAStates = topojson.feature(us, us.objects.USAStates);
+        console.log(usUSAStates);
+
+    //add out usCounties to the map
+        var state = map.append("path")
+          .datum(usUSAStates)
+          .attr("class", "state")
+          .attr("d", path);
     };
 };
 
