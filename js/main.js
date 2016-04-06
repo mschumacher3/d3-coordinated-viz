fe9bc50f863a848e2e79e03f01b116251254fcd9
mschumacher3 <mschumacher3@wisc.edu>
2016-04-05 21:10:08 -0500

    Module 9 in progress

--- js/main.js
+++ js/main.js
@@ -1,73 +1,53 @@
+//wrap everything in a self-executing anonymous function to move to local scope
+(function(){
 
 //begin script when window loads
 window.onload = setMap();
 
-//sets up choropleth map
+//set up choropleth map
 function setMap(){
-    //set width, height
-    var width=1050, 
-        height=800;
-        console.log("1");
+    //map frame dimensions
+    var width = 960,
+        height = 500;
 
-    //creates new svg container for the map
+    //create new svg container for the map
     var map = d3.select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
 
-
-    //create natural earth projection of world
-  var projection=d3.geo.naturalEarth()
-      .scale(2000)
-      .translate([width/2,height/2])
-   
-
+    //creates Albers equal area conic projection for the United States
+    //Still need to figure out how to get Hawaii and Alaska on here...
+    var projection = d3.geo.albers()
+        .rotate([96, 0])
+        .center([-.6, 38.7])
+        .parallels([29.5, 45.5])
+        .scale(1070)
+        .translate([width / 2, height / 2])
 
     var path = d3.geo.path()
         .projection(projection);
 
-    //use queue.js to parallelize asynchronous data loading
-    var q=d3_queue.queue()   
-        q.defer(d3.csv, "data/StateData.csv") //load attributes from csv
-        q.defer(d3.json, "data/USAStates.topojson") //load choropleth spatial data
-        q.await(callback);
-        console.log("2");
+    //uses queue.js to parallelize asynchronous data loading
+    d3_queue.queue()
+        //loads attributes from csv
+        .defer(d3.csv, "data/StateData.csv") 
+        //loads choropleth spatial data
+        .defer(d3.json, "data/USAStates.topojson") 
+        .await(callback);
 
-    
-    function callback(error, csvData, state){
-        
+    function callback(error, csvData, us){
+        //translates the states topojson
+        var usStates = topojson.feature(us, us.objects.USAStatesOnly);
+        console.log(usStates);
 
-        //apply graticule with lines 10 units apart in both dimensions--lat,lon
-        var graticule=d3.geo.graticule()
-            .step([10,10]);
-
-        //apply background to graticule
-        var gratBackground=map.append("path")
-            .datum(graticule.outline())
-            .attr("class","gratBackground")
-            .attr("d",path)
-
-        //add graticule lines
-        var gratLines=map.selectAll(".gratLines")
-            .data(graticule.lines())
-            .enter() 
-            .append("path")
-            .attr("class", "gratLines")
-            .attr("d",path);
-            //translate the states to topojson
-        var stateUSAStates = topojson.feature(state, state.objects.state);
-            //adds USAStates to the map
-        var selectStates=map.selectAll(".selectStates")
-            .data(stateUSAStates)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-              return "selectStates " + d.properties.adm1_code;
-
-            })
-            .attr("d",path);//assign d with attribute path
+        //add out usStates to the map
+        var states = map.append("path")
+          .datum(usStates)
+          .attr("class", "states")
+          .attr("d", path);
     };
 };
 
-
+})();
